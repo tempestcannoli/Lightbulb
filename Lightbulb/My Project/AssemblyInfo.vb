22d02579ca11912918014b143c05137fd30eee8e
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-02-11 21:33:06 +0200

    Add project files.

---
+++ Lightbulb/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Lightbulb")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("Lightbulb")>
+<Assembly: AssemblyCopyright("Copyright © HP 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8755e8ea-d8fb-4109-80d3-4e9c721ac84b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Lightbulb/UBulb.vb
@@ -0,0 +1,19 @@
+﻿Public Class UBulb
+    ' Klassisisene meetod kuvatava pildi määramiseks
+    Private Sub selectMood(ByVal intMood As Integer)
+        pbBulb.Image = imgloendbulb.Images(intMood)
+    End Sub
+    ' Avalik liidesmeetod
+    Public Sub setMood(ByVal strMood As String)
+        Select Case strMood
+            Case "off"
+                Me.selectMood(0)
+            Case "on"
+                Me.selectMood(1)
+            Case Else
+                Me.selectMood(0)
+        End Select
+    End Sub
+
+
+End Class
